6c4e11d830dcc80b3c30da7c755f76125d3a414c
Sumit Gandhi <sumitgandhi932@gmail.com>
2024-09-12 17:45:45 +0530

    delete vercel.json

--- config/db-connect.js
+++ config/db-connect.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose");
-
+const DB_URL = process.env.DB_URL;
 const connectDB = async () => {
   if (process.env.NODE_ENV === "production") {
     console.log("Using production database url");
     return;
   }
   console.log("Using development database url");
-
-  try {
-    mongoose
-      .connect(process.env.DB_URL)
-      .then(()=>
-        console.log("Database connected..."));
-  } catch (err) {
-    console.error("Error connecting to database:", err);
-  }
+  mongoose
+    .connect(DB_URL)
+    .then(() => console.log("Database connected..."))
+    .catch((error) => {
+      console.log(error.message);
+    });
 };
 
 module.exports = connectDB;
