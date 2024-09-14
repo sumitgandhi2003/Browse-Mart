54a5975f5a4ba928efe40ed52bb59f7d8f8c0a5c
Sumit Gandhi <sumitgandhi932@gmail.com>
2024-09-15 02:04:56 +0530

    remove some bugs in review card

--- src/Component/ReviewCard/ReviewCard.jsx
+++ src/Component/ReviewCard/ReviewCard.jsx
@@ -4,9 +4,9 @@ const ReviewCard = ({ review }) => {
   const { rating, heading, message } = review;
   return (
     <div className="bg-white relative p-2 border-2 border-dashed border-black/60  text-black min-w-[400px] min-h-[200px] max-h-[200px] max-w-[400px] mobile:w-full mobile:min-w-[100%] small-device:w-auto small-device:min-w-[400px] small-device:max-w-[400px]  rounded">
-      <div>{heading}</div>
+      <div className="font-roboto font-bold">{heading}</div>
       <div>{rating}</div>
-      <div>{message}</div>
+      <div className="font-roboto">{message}</div>
       <div className="font-roboto absolute bottom-1 right-2 font-bold">
         By Sumit Gandhi
       </div>
