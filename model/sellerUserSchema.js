4580988b2091ff36869c86a91156d6002cc80784
Sumit Gandhi <sumitgandhi932@gmail.com>
2024-11-10 01:56:58 +0530

    add sellerUserSchema for storing all required details of seller

--- model/sellerUserSchema.js
+++ model/sellerUserSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const sellerSchema = new mongoose.Schema({
-  user_id: {
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
